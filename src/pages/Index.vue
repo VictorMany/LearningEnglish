7c7f789abd3e3d56f117817a81e5b92a2cdc9a4e
Víctor Manuel Velázquez Fuentes <victor.velazfuentez@gmail.com>
2021-12-23 18:15:56 -0600

    new icons and images for home page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page style="background-color: #1575A1;"  class="row items-center justify-center">
+  <q-page  class="row items-center justify-center bg-indigo-3">
     <example-component
       title="Example component"
       active
